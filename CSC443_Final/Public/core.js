dcb0074f4979967607b8ad82e5a356105b1dddc1
boddenc1 <boddenc1@owls.southernct.edu>
2015-12-16 23:56:29 +0000

    no message

---
+++ CSC443_Final/Public/core.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Administrator on 12/16/2015.
+ */
+// public/core.js
+var link ="https://data.cityofchicago.org/resource/alternative-fuel-locations.json?";
+var app = angular.module('myApp', []);
+
+
+// function mainController($scope, $http) {
+//     $scope.formData = {};
+
+//     // when landing on the page, get all todos and show them
+//     $http.get(link)
+//         .success(function(data) {
+//             $scope.todos = data;
+//             console.log(data);
+//         })
+//         .error(function(data) {
+//             console.log('Error: ' + data);
+//         });
+
+// }
+
+
+
+
+
+
+
+
+
+// var app = angular.module('myApp', []);
+
+// app.controller('ctrl', function ($scope, $http) {
+// $http.get(link)
+//         .then(function (response) {
+//             $scope.record = response.data;});
+// });
